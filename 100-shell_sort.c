d4866ef078fd649cef16f24f361c5c5c1b206195
xola-mthembu <xola.mthembu@gmail.com>
2024-02-20 11:55:48 -0800

    Task4

---
+++ 100-shell_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * shell_sort - sorts an array of integers in ascending order using the
+ * Shell sort algorithm, Knuth sequence
+ * @array: The array to be sorted
+ * @size: The size of the array
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t gap = 1, i, j;
+	int temp;
+
+	if (!array || size < 2)
+		return;
+
+	/* Calculate the maximum gap with Knuth's formula */
+	while (gap < size / 3)
+		gap = gap * 3 + 1;
+
+	while (gap > 0)
+	{
+		for (i = gap; i < size; i++)
+		{
+			temp = array[i];
+			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+			{
+				array[j] = array[j - gap];
+			}
+			array[j] = temp;
+		}
+		print_array(array, size); /* Print array after each interval decrease */
+		gap /= 3; /* Decrease the gap */
+	}
+}
