891c1232e47720784140687a4bb00fb003660cb5
xola-mthembu <xola.mthembu@gmail.com>
2024-02-20 15:29:16 -0800

    Task 7 Fixed

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,38 +1,60 @@
 #include "sort.h"
+#include <stdio.h>
+
+/**
+ * print_array_portion - Prints a portion of an array
+ * @array: The array to be printed
+ * @begin: The starting index
+ * @end: The ending index, exclusive
+ */
+void print_array_portion(const int *array, int begin, int end)
+{
+	int i;
+
+	for (i = begin; i < end; i++)
+	{
+		if (i > begin)
+			printf(", ");
+		printf("%d", array[i]);
+	}
+	printf("\n");
+}
 
 /**
  * merge - Merges two subarrays of array.
  * @array: Array to be sorted.
- * @left: Temporary array to store left subarray.
- * @right: Temporary array to store right subarray.
- * @size: Number of elements in array.
+ * @left: Temporary array for the left subarray.
+ * @right: Temporary array for the right subarray.
+ * @start: Start index of the left subarray in the main array.
+ * @mid: Middle index, separating left and right subarrays.
+ * @end: End index of the right subarray in the main array.
  */
-void merge(int *array, int *left, int *right, size_t size)
+void merge(int *array, int *left, int *right, int start, int mid, int end)
 {
-	int i = 0, j = 0, k = 0;
-	size_t mid = size / 2;
+	int i = 0, j = 0, k = start;
+	int left_size = mid - start, right_size = end - mid;
 
-	while (i < (int)mid && j < (int)(size - mid))
+	printf("Merging...\n[left]: ");
+	print_array_portion(left, 0, left_size);
+	printf("[right]: ");
+	print_array_portion(right, 0, right_size);
+
+	while (i < left_size && j < right_size)
 	{
-		if (left[i] < right[j])
-		{
+		if (left[i] <= right[j])
 			array[k++] = left[i++];
-		}
 		else
-		{
 			array[k++] = right[j++];
-		}
 	}
 
-	while (i < (int)mid)
-	{
+	while (i < left_size)
 		array[k++] = left[i++];
-	}
 
-	while (j < (int)(size - mid))
-	{
+	while (j < right_size)
 		array[k++] = right[j++];
-	}
+
+	printf("[Done]: ");
+	print_array_portion(array, start, end);
 }
 
 /**
@@ -40,7 +62,7 @@ void merge(int *array, int *left, int *right, size_t size)
  * @buffer: Buffer array for merging.
  * @start: Starting index of subarray to sort.
  * @end: Ending index of subarray to sort.
- * @array: The original array to be sorted.
+ * @array: Original array to be sorted.
  */
 void top_down_split_merge(int *buffer, size_t start, size_t end, int *array)
 {
@@ -52,16 +74,14 @@ void top_down_split_merge(int *buffer, size_t start, size_t end, int *array)
 	mid = (start + end) / 2;
 	top_down_split_merge(array, start, mid, buffer);
 	top_down_split_merge(array, mid, end, buffer);
-	merge(buffer + start, array + start, array + mid, end - start);
+	merge(buffer, array + start, array + mid, start, mid, end);
 
 	for (i = start; i < end; i++)
-	{
 		array[i] = buffer[i];
-	}
 }
 
 /**
- * merge_sort - Sorts an array in asc order using the Merge sort algorithm.
+ * merge_sort - Sorts an array using the Merge sort algorithm.
  * @array: The array to be sorted.
  * @size: The size of the array.
  */
@@ -78,9 +98,7 @@ void merge_sort(int *array, size_t size)
 		return;
 
 	for (i = 0; i < size; i++)
-	{
 		buffer[i] = array[i];
-	}
 
 	top_down_split_merge(buffer, 0, size, array);
 	free(buffer);
