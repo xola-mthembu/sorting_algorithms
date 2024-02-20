04f1323ade99d6f647a0db6d5f3a725b00dc305e
xola-mthembu <xola.mthembu@gmail.com>
2024-02-20 13:20:29 -0800

    Task6 updated - gawd

--- sort.h
+++ sort.h
@@ -9,7 +9,8 @@
  * @n: Integer stored in the node
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
- * Description: Doubly linked list node structure for project
+ * Description: Doubly linked list node structure
+ * for Holberton project
  */
 typedef struct listint_s
 {
@@ -26,6 +27,13 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
-void counting_sort(int *array, size_t size); /* Add this line */
+
+/**
+ * counting_sort - Sorts an array of integers in ascending order
+ * using the Counting sort algorithm.
+ * @array: The array to be sorted.
+ * @size: The size of the array.
+ */
+void counting_sort(int *array, size_t size);
 
 #endif /* SORT_H */
